e00d9825347cad65a2701b5f97206f9b2aa4335b
unknown <ahgusdl288@naver.com>
2023-04-18 18:52:19 +0900

    Feat: Apply Aop, AopSkip

--- src/main/java/com/controller/MemberController.java
+++ src/main/java/com/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.service.MemberService;
 
@@ -10,5 +12,14 @@ public class MemberController {
 
 	@Autowired
 	MemberService service;
+	
+	// 세션 아웃
+	@GetMapping("sessionOut.do")
+	public String sessionOut(Model model) {
+		String sessionOut = "logout";
+		model.addAttribute("sessionOut", sessionOut);
+		
+		return "message";
+	}
 	
 }
--- src/main/java/com/controller/PostController.java
+++ src/main/java/com/controller/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.aop.AopSkip;
 import com.service.PostService;
 
 @Controller
@@ -13,9 +14,10 @@ public class PostController {
 	PostService service;
 	
 	// 메인 화면
+	@AopSkip
 	@GetMapping("main.do")
 	public String goMain() {
-		return "Main";
+		return "main";
 	}
 	
 }
