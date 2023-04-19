b7c2aecc7c6286249871ed5345f176965569a0be
unknown <ahgusdl288@naver.com>
2023-04-19 18:26:26 +0900

    Feat: Add charInfo, freePostList, login, logout, my page btn / link, Create CharInfoController, jsp files - Add charInfo, freePostList, login, logout, memberUpdate mappings and jsp files - Create CharInfoController - Aop doesn't work

--- src/main/java/com/controller/MemberController.java
+++ src/main/java/com/controller/MemberController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.aop.AopSkip;
 import com.service.MemberService;
 
 @Controller
@@ -14,12 +16,32 @@ public class MemberController {
 	MemberService service;
 	
 	// 세션 아웃
+	@AopSkip
 	@GetMapping("sessionOut.do")
 	public String sessionOut(Model model) {
 		String sessionOut = "logout";
 		model.addAttribute("sessionOut", sessionOut);
 		
 		return "message";
+	}
+	
+	// 로그인
+	@AopSkip
+	@GetMapping("login.do")
+	public String login() {
+		return "login";
+	}
+	
+	// 로그아웃
+	@GetMapping("logout.do")
+	public String logout() {
+		return "main";
+	}
+	
+	// 마이 페이지 - 내 정보 수정
+	@GetMapping("memberUpdate.do")
+	public String memberUpdate() {
+		return "memberUpdate";
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/views/freePostList.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>자유 게시판</title>
+</head>
+<body>
+	<h1>this is freePostList.jsp</h1>
+</body>
+</html>
